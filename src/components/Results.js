574a1de6a5099777cfe0b490fbfdba9218f1ef46
sidseal <sidseal@me.com>
2020-07-30 16:41:12 -0400

    fixes

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,42 @@
 import React from "react";
 import axios from 'axios';
-
+import Search from "./Search"
 
 function Results() {
     const [people, setPeople] = React.useState([])
+    const [searchVal, setSearchVal] = React.useState([])
 
     React.useEffect(() => {
         axios.get("https://randomuser.me/api/?results=10&inc=name,phone,nat,email,picture").then(x => setPeople(x.data.results))
     }, [])
+
+    function sortResults(){
+        let newPeople = new Array(...people)
+        newPeople.sort(function(a, b) {
+            if (a.name.first > b.name.first) {
+              return 1;
+            }
+            if (a.name.first < b.name.first) {
+              return -1;
+            }
+            return 0;
+          });
+        setPeople(newPeople)
+
+    }
+    const handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+    
+        // Updating the input's state
+        setSearchVal(event.target.value);
+      };
     return (
-        <div className="results">
+        <>
+        <Search handleSearchChange={handleInputChange}/>
+        <p>{searchVal}</p>
+        <div onClick={()=>sortResults()}
+        className="results">
             {people.map(person => {
                 console.log(person)
                 return (
@@ -17,8 +44,8 @@ function Results() {
                 )
 
             })
-            })</div>
-
+            }</div>
+</>
     )
 }
 
